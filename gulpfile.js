178c04f88c5ca670d3a414e9e4b179ef1f80b09f
TJ Egan <twegan@us.ibm.com>
2016-09-12 22:16:38 -0500

    add video, more gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var htmlmin     = require('gulp-htmlmin');
 var rename      = require("gulp-rename");
 var imagemin    = require('gulp-imagemin');
 var ghPages     = require('gulp-gh-pages');
+var babel       = require("gulp-babel");
+var uglify      = require('gulp-uglify');
 
 gulp.task('html', function() {
   return gulp.src('./app/index.html')
@@ -22,8 +24,13 @@ gulp.task('assets', function() {
     .pipe(gulp.dest('./dist/img'))
 })
 
+gulp.task('copy', function() {
+  gulp.src('./app/assets/*')
+  .pipe(gulp.dest('./dist/assets'))
+})
+
 gulp.task('sass', function() {
-    return gulp.src('./app/scss/main.scss')
+    return gulp.src('./app/scss/**/*.scss')
       .pipe(maps.init())
       .pipe(sass())
       .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
@@ -33,6 +40,16 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('./dist/css'))
       .pipe(browserSync.reload({stream: true}))
 })
+
+var gulp = require("gulp");
+
+
+gulp.task('js', function () {
+  return gulp.src('./app/js/app.js')
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+});
 
 gulp.task('deploy', function() {
   return gulp.src('./dist/**/*')
@@ -47,7 +64,8 @@ gulp.task('serve', ['build'], function() {
     }
   })
   gulp.watch('./app/scss/**/*.scss', ['sass'])
+  gulp.watch('./app/js/*.js', ['js']).on('change', browserSync.reload)
   gulp.watch('./app/*.html', ['html']).on('change', browserSync.reload)
 })
 
-gulp.task('build', ['sass', 'html'])
+gulp.task('build', ['copy', 'assets', 'sass', 'js', 'html'])
