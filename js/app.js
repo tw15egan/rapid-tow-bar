9c6b3042dbc7ed3fe9b0b9128497365a0d6c573a
TJ Egan <twegan@us.ibm.com>
2016-09-13 18:39:55 -0500

    Update 2016-09-13T23:39:49.750Z

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', e => {
+  if (window.scrollY > 99) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+});
