b1d56356f2371b703800d0b925cc1ec3fd97d162
TJ Egan <twegan@us.ibm.com>
2016-10-01 13:12:15 -0500

    Update 2016-10-01T18:12:14.467Z

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 const toggle = document.querySelector('.header__toggle');
+// const header = document.querySelector('.header');
+// 
+// window.addEventListener('scroll', function() {
+//   if (window.pageYOffset > 70) {
+//     header.classList.add('scroll');
+//   } else {
+//     header.classList.remove('scroll');
+//   }
+// })
 
 toggle.addEventListener('click', function () {
   toggle.classList.toggle('active');
