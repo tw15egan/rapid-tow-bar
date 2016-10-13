c9e72db1e0325274547532c14334cb68f0af47ac
TJ Egan <twegan@us.ibm.com>
2016-10-12 19:44:40 -0500

    fix responsive issue

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,18 @@
 const toggle = document.querySelector('.header__toggle');
-// const header = document.querySelector('.header');
-// 
-// window.addEventListener('scroll', function() {
-//   if (window.pageYOffset > 70) {
-//     header.classList.add('scroll');
-//   } else {
-//     header.classList.remove('scroll');
-//   }
-// })
 
 toggle.addEventListener('click', function() {
   toggle.classList.toggle('active');
 });
+
+// const link = document.querySelectorAll('.header__link');
+// 
+// function addPadding() {;
+//   
+// }
+// 
+// [...link].forEach((item) => {
+//   item.addEventListener('click', () => {
+//     setTimeout(window.scrollTo(window.scrollX, window.scrollY - 70), 200);
+//   });
+// });
+
