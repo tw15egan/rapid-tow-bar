100022946a522aa39769df0b33609bb9e4df6f94
TJ Egan <twegan@us.ibm.com>
2016-10-12 20:11:12 -0500

    Update 2016-10-13T01:11:10.986Z

--- js/app.js
+++ js/app.js
@@ -1,17 +1,1 @@
-const toggle = document.querySelector('.header__toggle');
-
-toggle.addEventListener('click', function () {
-  toggle.classList.toggle('active');
-});
-
-// const link = document.querySelectorAll('.header__link');
-// 
-// function addPadding() {;
-//   
-// }
-// 
-// [...link].forEach((item) => {
-//   item.addEventListener('click', () => {
-//     setTimeout(window.scrollTo(window.scrollX, window.scrollY - 70), 200);
-//   });
-// });
+"use strict";var toggle=document.querySelector(".header__toggle");toggle.addEventListener("click",function(){toggle.classList.toggle("active")});
